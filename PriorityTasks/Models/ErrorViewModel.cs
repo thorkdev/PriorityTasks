f94acaf8c3612d917e2d17cf4e777707a22089a6
Thork <contact@thorknox.dev>
2021-08-20 14:54:22 -0500

    Add project files.

---
+++ PriorityTasks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PriorityTasks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
